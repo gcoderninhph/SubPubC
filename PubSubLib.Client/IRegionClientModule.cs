b3d312829c465f177aedf289f0890b9620564ea5
DESKTOP-F72T0IU\ADMIN <gcoder.ninhph@gmail.com>
2026-07-03 10:39:27 +0700

    fix

--- PubSubLib.Client/IRegionClientModule.cs
+++ PubSubLib.Client/IRegionClientModule.cs
@@ -6,7 +6,7 @@ public interface IRegionClientModule : IClientModule
 {
     static IRegionClientModule Create(Config config)
     {
-        throw new NotImplementedException();
+        return RegionClientModule.Create(config);
     }
 
     // Lấy ra 1 đối tượng T cụ thể thông qua id của đối tượng
@@ -16,7 +16,9 @@ public interface IRegionClientModule : IClientModule
 
     // Khi trên server tạo 1 đối tượng Mirror tương ứng với T qua IRegionModule.CreateUnit -> 
     void OnCreateUnit<T, TR>(Func<T, TR> unit) where T : IRegionUnit<TR>;
+    // Xóa T thủ công, khi xóa sẽ dọn sạch cả 
     void Destroy<T, TR>(T unit) where T : IRegionUnit<TR>;
     // Tương tự như IPubSubClient.Tick(), nó sẽ gửi lệnh watcher ping unit lên server nhằm đồng bộ các unit trên bản đồ
     void Tick();
+    void MoveWatcher(Vector2 position, float range);
 }
