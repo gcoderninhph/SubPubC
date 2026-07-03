895eb4d023d7fcec9595b49140c9d15cbed4c788
DESKTOP-F72T0IU\ADMIN <gcoder.ninhph@gmail.com>
2026-07-03 10:38:07 +0700

    fix

--- PubSubLib.Client/IRegionUnit.cs
+++ PubSubLib.Client/IRegionUnit.cs
@@ -15,7 +15,7 @@
 //          IRegionClientModule.Destroy/event Destroy từ server
 // IRegionOnDestroy cho phép T triển khai hàm OnUnitDestroy, hàm này sẽ chạy ngay khi sự kiện destroy IRegionUnit xảy ra
 //          khi IRegionClientModule.Destroy/event Destroy từ server
-//          Luồng: IRegionClientModule.Destroy/event Destroy từ server -> IRegionOnDestroy.OnCreateUnit (nếu có)
+//          Luồng: IRegionClientModule.Destroy/event Destroy từ server -> IRegionOnDestroy.OnDestroyUnit (nếu có)
 // ISetRegionUnit cho phép T được set IRegionUnit đang giữ nó
 //          Luồng: IRegionClientModule.OnCreateUnit -> ISetRegionUnit.SetRegionUnit (nếu có) -> IRegionUnitOnStart.OnUnitStart (nếu có)
 // IRegionUnitOnStart cho phép T triên khai hàm OnUnitStart, hàm này sẽ chạy ngay sau khi IRegionClientModule.OnCreateUnit
@@ -30,6 +30,8 @@ public interface IRegionUnit<T>
     // Khi generate sẽ tạo ra 1 string static _type, được generate từ attribute (mặc định lấy tên của class)
     // nếu trong proto buff cũng có trường type thì trường mirror type của proto sẽ là _Type
     string Type { get; }
+    long Id { get; }
+
     // get đối tượng T mà nó giữ thông qua tham chiếu của IUnit.Target -> parse to T
     T Get();
 }
